546c85ba18786fe68c4a61fb227283cb10ed08b8
Tibor5 <tibibibi.novakovic9@gmail.com>
2024-04-16 08:30:53 +0200

    @ Arch - Scripts - chore(clean): remove unused.

--- .scripts/wayland_sleep.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-swayidle -w timeout 600 'swaylock -f' \
-  timeout 900 'hyprctl dispatch dpms off' \
-  timeout 900 'systemctl suspend' \
-  resume 'hyprctl dispatch dpms on' \
-  before-sleep 'swaylock -f' &
