28260a80a8ee2920c550a95ec3a580089066d9c1
Tibor5 <tibibibi.novakovic9@gmail.com>
2024-05-27 21:19:39 +0200

    @ Arch - Fish - chore(alias): cleanup. Neovim - feat(notes): markdown preview. Scripts - chore: clean unused.

--- .config/nvim/lua/plugins/notes.lua
+++ .config/nvim/lua/plugins/notes.lua
@@ -1,65 +1,12 @@
 return {
   {
-    "iamcco/markdown-preview.nvim",
-    build = function() vim.fn["mkdp#util#install"]() end,
-    ft = { "markdown" },
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    keys = {
-      { "<leader>mp", "<cmd>MarkdownPreview<CR>", desc = "[m]arkdown [p]review" },
-      { "<leader>ms", "<cmd>MarkdownPreviewStop<CR>", desc = "[m]arkdown [s]top" },
-    },
-    config = function()
-      -- default: 0
-      vim.g.mkdp_auto_start = 0
-      -- default: 1
-      vim.g.mkdp_auto_close = 1
-      -- default: 0
-      vim.g.mkdp_refresh_slow = 1
-      -- default: 0
-      vim.g.mkdp_command_for_global = 0
-      -- default: 0
-      vim.g.mkdp_open_to_the_world = 0
-      -- default: empty
-      vim.g.mkdp_open_ip = ""
-      -- default: ""
-      vim.g.mkdp_browser = "firefox"
-      -- default: 0
-      vim.g.mkdp_echo_preview_url = 0
-      -- default: empty
-      vim.g.mkdp_browserfunc = ""
-      vim.g.mkdp_preview_options = {
-        ["mkit"] = {},
-        ["katex"] = {},
-        ["uml"] = {},
-        ["maid"] = {},
-        ["disable_sync_scroll"] = 0,
-        ["sync_scroll_type"] = "middle",
-        ["hide_yaml_meta"] = 0,
-        ["sequence_diagrams"] = {},
-        ["flowchart_diagrams"] = {},
-        ["content_editable"] = false,
-        ["disable_filename"] = 1,
-        ["toc"] = {},
-      }
-      -- Absolute path or expand("~/markdown.css")
-      vim.g.mkdp_markdown_css = ""
-      -- Absolute path or expand("~/highlight.css")
-      vim.g.mkdp_highlight_css = ""
-      -- use a custom port to start server or empty for random
-      vim.g.mkdp_port = ""
-      vim.g.mkdp_page_title = "「${name} 」"
-      -- use a custom location for images
-      -- vim.g.mkdp_images_path = "/home/tibor/"
-      vim.g.mkdp_filetypes = { "markdown" }
-      vim.g.mkdp_theme = "dark"
-      -- set vim.g.mkdp_auto_close = 0 if this is enabled
-      -- default: 0
-      vim.g.mkdp_combine_preview = 0
-      -- auto refetch combine preview contents when change markdown buffer
-      -- only when g:mkdp_combine_preview is 1
-      vim.g.mkdp_combine_preview_auto_refresh = 1
-    end,
+    "henriklovhaug/Preview.nvim",
+    ft = "markdown",
+    cmd = "Preview",
+    keys = { { "<leader>mp", "<cmd>Preview<CR>", desc = "[m]arkdown [p]review" } },
+    config = true,
   },
+  { "ellisonleao/glow.nvim", ft = "markdown", cmd = "Glow", config = { border = "single" } },
   {
     "epwalsh/obsidian.nvim",
     version = "*",
--- .local/bin/install_terminfo_alacritty.bash
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-curl -sSL https://raw.githubusercontent.com/alacritty/alacritty/master/extra/alacritty.info | sudo tic -x -
