b1b1120940059963585dc64cd78d8e2bea466cab
Tibor5 <tibibibi.novakovic9@gmail.com>
2024-06-26 14:04:59 +0200

    @ Arch - dump.

--- .config/fish/functions/fissh.fish
+++ .config/fish/functions/fissh.fish
@@ -1,3 +1,3 @@
-function ssh_agent --description "Activate SSH agent with given key"
+function fissh --description "Activate SSH agent and activate the given key."
     eval (ssh-agent -c) && ssh-add $HOME/.ssh/$argv
 end
