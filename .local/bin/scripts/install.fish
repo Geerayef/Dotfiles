30e852fd84b5c78772b71c1e651d84581d06b3cd
Tibor Novakovic <tibibibi.novakovic9@gmail.com>
2024-08-20 22:15:29 +0200

    @ Arch - Foot - feat(keys): Search mode delete word backwards with Ctrl+w. Scripts - chore(install): Add notify logs. feat(install): Check for validity of component script, early exit on no match.

--- .local/bin/scripts/install.fish
+++ .local/bin/scripts/install.fish
@@ -1,6 +1,7 @@
 #!/usr/bin/env fish
 
 function main -d "Install user-space component for which there is a script."
+    printf "~~~~~ %s\n" "Install user-space component for which there is a script."
     set -l component (string lower "$argv[1]")
     set -l dir_scripts (status dirname)
     set -l installables (fd "install_" -t f $dir_scripts)
@@ -16,9 +17,13 @@ function main -d "Install user-space component for which there is a script."
             break
         end
     end
+    if test (string length "$component_script") -eq 0
+        notify ERROR "Selected component ($component) is invalid."
+        return 1
+    end
     notify INFO "Installing $component."
     exec $component_script
-    notify DONE "Install $component"
+    notify DONE "Install $component."
 end
 
 main $argv
