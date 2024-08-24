e59f4e046df5d4817823abd390047213e7e6d3ae
Tibor Novakovic <tibibibi.novakovic9@gmail.com>
2024-08-24 18:16:15 +0200

    @ Arch - Git - chore(ignore): nnn/.lastd. Scripts - feat(ss): Add font, color and show dimensions of screenshot selection

--- .local/bin/scripts/ss.bash
+++ .local/bin/scripts/ss.bash
@@ -6,10 +6,10 @@ main() {
   name_template="$screenshots/Screenshot $(date '+%d-%m-%Y %T').png"
   case "$1" in
     --selection)
-      slurp | grim -g- -s1 "$name_template"
+      slurp -d -c "#F9E2AF" -F "Iosevka Nerd Font Mono 20" -w 2 | grim -g- -s1 -t png "$name_template"
       ;;
     *)
-      grim -s1 "$name_template"
+      grim -s1 -t png "$name_template"
       ;;
   esac
 }
