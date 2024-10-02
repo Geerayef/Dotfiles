492c7bf37d52db86ba630643323878d280195b4e
Tibor Novakovic <tibibibi.novakovic9@gmail.com>
2024-10-02 09:05:22 +0200

    @ Arch - Fish - feat(alias): Replace package manager aliases with abbrs.

--- .config/fish/conf.d/alias.fish
+++ .config/fish/conf.d/alias.fish
@@ -1,4 +1,4 @@
-# ~  General
+# ~ General
 
 alias C="clear"
 if test -e "$(command -v eza)"
@@ -22,12 +22,12 @@ else
     alias ll="ls -la --color=always --group-directories-first"
 end
 
-# ~  Apps
+# ~ Apps
 
 alias v="nvim"
 alias nv="neovide"
 
-# ~  Git
+# ~ Git
 
 alias dot="/usr/bin/git --git-dir=$HOME/Dotfiles/ --work-tree=$HOME"
 alias dotu="dot add .config/{fish,bash,zsh,starship,alacritty,wezterm,foot,kitty,tmux,hypr,nvim,vim,neovide,moxide,mdt,glow,emacs,tofi,firefoxcss,nnn,bat,btop,mako,fontconfig,vimiumc,zathura,zed,ruff}/ .local/bin/ .zshenv .gitignore"
@@ -50,55 +50,55 @@ alias gch="git checkout"
 alias gb="git branch"
 alias gl='git log --pretty="%h » ¦ « %aN » ¦ « %s » ¦ « %aD"'
 
-# ~  Package managers
+# ~ Package managers
 
 if test $SYSTEM_PACKAGE_MANAGER = apt
-    alias ad="sudo apt update"
-    alias ag="sudo apt upgrade"
-    alias auu="sudo apt update && sudo apt upgrade"
-    alias ai="sudo apt install"
-    alias ari="sudo apt reinstall"
-    alias ar="sudo apt remove"
-    alias aar="sudo apt autoremove"
-    alias ap="sudo apt purge"
-    alias ac="sudo apt clean"
-    alias aac="sudo apt autoclean"
-    alias as="sudo apt search"
-    alias ash="sudo apt show"
-    alias al="sudo apt list"
-    alias agd="sudo apt-get update"
-    alias agg="sudo apt-get upgrade"
-    alias aguu="sudo apt-get update && sudo apt-get upgrade"
-    alias agi="sudo apt-get install"
-    alias agri="sudo apt-get reinstall"
-    alias agr="sudo apt-get remove"
-    alias agp="sudo apt-get purge"
-    alias agar="sudo apt-get autoremove"
-    alias agssfy="sudo apt-get satisfy"
-    alias agc="sudo apt-get clean"
-    alias agac="sudo apt-get autoclean"
-    alias agchk="sudo apt-get check"
-    alias agsrc="sudo apt-get source"
-    alias agdl="sudo apt-get download"
+    abbr -a ad "sudo apt update"
+    abbr -a ag "sudo apt upgrade"
+    abbr -a auu "sudo apt update && sudo apt upgrade"
+    abbr -a ai "sudo apt install"
+    abbr -a ari "sudo apt reinstall"
+    abbr -a ar "sudo apt remove"
+    abbr -a aar "sudo apt autoremove"
+    abbr -a ap "sudo apt purge"
+    abbr -a ac "sudo apt clean"
+    abbr -a aac "sudo apt autoclean"
+    abbr -a as "sudo apt search"
+    abbr -a ash "sudo apt show"
+    abbr -a al "sudo apt list"
+    abbr -a agd "sudo apt-get update"
+    abbr -a agg "sudo apt-get upgrade"
+    abbr -a aguu "sudo apt-get update && sudo apt-get upgrade"
+    abbr -a agi "sudo apt-get install"
+    abbr -a agri "sudo apt-get reinstall"
+    abbr -a agr "sudo apt-get remove"
+    abbr -a agp "sudo apt-get purge"
+    abbr -a agar "sudo apt-get autoremove"
+    abbr -a agssfy "sudo apt-get satisfy"
+    abbr -a agc "sudo apt-get clean"
+    abbr -a agac "sudo apt-get autoclean"
+    abbr -a agchk "sudo apt-get check"
+    abbr -a agsrc "sudo apt-get source"
+    abbr -a agdl "sudo apt-get download"
 else if test $SYSTEM_PACKAGE_MANAGER = dnf
-    alias dnfu="sudo dnf upgrade --refresh"
-    alias dnfi="sudo dnf install"
-    alias dnfgi="sudo dnf groupinstall"
-    alias dnfr="sudo dnf remove"
-    alias dnfar="sudo dnf autoremove"
-    alias dnfgr="sudo dnf groupremove"
-    alias dnfc="sudo dnf clean all"
-    alias dnfs="sudo dnf search"
-    alias dnfl="sudo dnf list"
-    alias dfnli="sudo dnf list installed"
-    alias dnfgl="sudo dnf grouplist"
-    alias dnfh="dnf help"
+    abbr -a dnfu "sudo dnf upgrade --refresh"
+    abbr -a dnfi "sudo dnf install"
+    abbr -a dnfgi "sudo dnf groupinstall"
+    abbr -a dnfr "sudo dnf remove"
+    abbr -a dnfar "sudo dnf autoremove"
+    abbr -a dnfgr "sudo dnf groupremove"
+    abbr -a dnfc "sudo dnf clean all"
+    abbr -a dnfs "sudo dnf search"
+    abbr -a dnfl "sudo dnf list"
+    abbr -a dfnli "sudo dnf list installed"
+    abbr -a dnfgl "sudo dnf grouplist"
+    abbr -a dnfh "dnf help"
 else if test $SYSTEM_PACKAGE_MANAGER = pacman
-    alias pi="sudo pacman -S --needed"
-    alias pu="sudo pacman -Syu"
-    alias pr="sudo pacman -Rs"
-    alias pq="sudo pacman -Q"
-    alias ps="sudo pacman -Ss"
+    abbr -a pi "sudo pacman -S --needed"
+    abbr -a pu "sudo pacman -Syu"
+    abbr -a pr "sudo pacman -Rs"
+    abbr -a pq "sudo pacman -Q"
+    abbr -a ps "sudo pacman -Ss"
 else
-    echo "Please set the SYSTEM_PACKAGE_MANAGER environment variable to the name of your system package manager."
+    echo "[ ERROR ] | Please set the SYSTEM_PACKAGE_MANAGER environment variable to the name of your system package manager."
 end
