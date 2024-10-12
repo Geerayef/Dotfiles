4ae7af2395343a88fdc6fd9228aef06c8b1d6734
Tibor Novakovic <tibibibi.novakovic9@gmail.com>
2024-10-12 21:42:22 +0200

    @ Arch - Fish - feat(ssh): Use GPG agent SSH socket for managing SSH keys.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,16 +1,5 @@
 set -U fish_greeting
 
-if status is-login
-    if test -r "$SSH_ENV"
-        . $SSH_ENV >/dev/null
-    else
-        fissh gh >/dev/null 2>/dev/null
-        if test $status -ne 0
-            notify ERROR "Failed to start SSH agent with default key."
-        end
-    end
-end
-
 if status is-interactive
     function fish_user_key_bindings
         fish_default_key_bindings
--- .config/fish/functions/fissh.fish
+++ .config/fish/functions/fissh.fish
@@ -1,7 +1,3 @@
 function fissh --description "Activate SSH agent and activate the given key."
-    # eval (ssh-agent -c) && ssh-add $HOME/.ssh/$argv
-    ssh-agent -c | sed 's/^echo/#echo/' >$SSH_ENV
-    chmod 600 $SSH_ENV
-    . $SSH_ENV >/dev/null
-    ssh-add $HOME/.ssh/$argv
+    eval (ssh-agent -c) && ssh-add $HOME/.ssh/$argv
 end
