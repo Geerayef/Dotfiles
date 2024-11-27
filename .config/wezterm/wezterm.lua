36b124e725dada69797405c119b0a3c228c609ef
Tibor Novakovic <tibibibi.novakovic9@gmail.com>
2024-11-27 14:30:39 +0100

    @ Arch - Wezterm - theme.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -196,7 +196,7 @@ C.force_reverse_video_cursor = false
 -- Unsifted Wheat (terminal.sexy)
 -- BRIGHT
 -- Gruvbox Material (Gogh) | Shic (terminal.sexy)
-C.color_scheme = "VWbug (terminal.sexy)"
+C.color_scheme = "Black Metal (Marduk) (base16)"
 C.colors = {
   background = kngw.palette.dragonInk1,
   cursor_fg = kngw.ansi[1],
