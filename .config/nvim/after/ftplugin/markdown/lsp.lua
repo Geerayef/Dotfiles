86c170b57ac6290824c6ef577b50c074b0b2ee6e
Tibor Novakovic <tibibibi.novakovic9@gmail.com>
2025-03-05 21:08:43 +0100

    @ Arch - Neovim - fix(ft.markdown): Don't start LSP on markdown documentation popups.

--- .config/nvim/after/ftplugin/markdown/lsp.lua
+++ .config/nvim/after/ftplugin/markdown/lsp.lua
@@ -5,9 +5,11 @@ local markdown_oxide = {
   cmd = { "markdown-oxide" },
 }
 
-vim.schedule(function()
-  vim.api.nvim_win_call(
-    vim.api.nvim_get_current_win(),
-    function() require("util.lsp").start(markdown_oxide) end
-  )
-end)
+if string.match(vim.api.nvim_buf_get_name(0), ".md", 1) == ".md" then
+  vim.schedule(function()
+    vim.api.nvim_win_call(
+      vim.api.nvim_get_current_win(),
+      function() require("util.lsp").start(markdown_oxide) end
+    )
+  end)
+end
--- .config/nvim/after/ftplugin/markdown/markdown.lua
+++ .config/nvim/after/ftplugin/markdown/markdown.lua
@@ -1,5 +1,5 @@
 vim.o.wrap = true
-vim.o.wrapmargin = 80
+vim.o.textwidth = 80
 vim.o.spell = true
 vim.o.spelllang = "en_gb"
 vim.o.spelloptions = "camel"
