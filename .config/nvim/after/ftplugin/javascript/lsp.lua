a3d26437100d10fdafb70c2c5234bb1c16872abd
Tibor Novakovic <tibibibi.novakovic9@gmail.com>
2025-04-20 00:27:17 +0200

    @ Arch - Neovim - chore(after.lsp): JavaScript TypeScriptLSP.

--- .config/nvim/after/ftplugin/javascript/lsp.lua
+++ .config/nvim/after/ftplugin/javascript/lsp.lua
@@ -21,9 +21,23 @@ local biome = {
   cmd = { "biome", "lsp-proxy" },
 }
 
+local tslsp = {
+  on_attach = require("core.func").LSPAttach,
+  filetypes = { "javascript", "typescript" },
+  root_patterns = { "package.json", "*.js", "*.ts" },
+  cmd = { "typescript-language-server", "--stdio" },
+}
+
 vim.schedule(function()
   vim.api.nvim_win_call(
     vim.api.nvim_get_current_win(),
     function() require("util.lsp").start(biome) end
+  )
+end)
+
+vim.schedule(function()
+  vim.api.nvim_win_call(
+    vim.api.nvim_get_current_win(),
+    function() require("util.lsp").start(tslsp) end
   )
 end)
