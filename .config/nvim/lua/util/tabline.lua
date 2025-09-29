0aa88ee7c9c9d0550d0bd794b8c45e29704ca791
Tibor Novakovic <tibor.novakovic@isis-papyrus.com>
2025-09-29 10:49:47 +0200

    fix(util.tabline): Show modified only for modified buffers. Keep active buffer name when switching tabs

--- .config/nvim/lua/util/tabline.lua
+++ .config/nvim/lua/util/tabline.lua
@@ -1,13 +1,22 @@
 GRIM = {}
 
-local function get_buf_name(buf)
+local last_active_win = {}
+
+vim.api.nvim_create_autocmd("WinEnter", {
+  callback = function()
+    last_active_win[vim.api.nvim_get_current_tabpage()] =
+      vim.api.nvim_get_current_win()
+  end,
+})
+
+local get_buf_name = function(buf)
   local buf_name = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(buf), ":t")
   local buf_type = vim.bo[buf].buftype
   local buf_ft = vim.bo[buf].filetype
   if buf_type == "help" then
     return "Help: " .. buf_name
   elseif buf_ft == "oil" then
-    return "Oil: " .. (buf_name ~= "" and buf_name or ".")
+    return "Oil: " .. (buf_name ~= "" and buf_name or vim.fn.getcwd())
   elseif buf_type == "terminal" then
     return "Terminal"
   elseif buf_type == "quickfix" then
@@ -18,11 +27,15 @@ local function get_buf_name(buf)
   return buf_name
 end
 
-local function get_normal_wins(tab)
+local get_normal_wins = function(tab)
   return vim.tbl_filter(
     function(win) return vim.api.nvim_win_get_config(win).relative == "" end,
     vim.api.nvim_tabpage_list_wins(tab)
   )
+end
+
+local tab_active_p = function(tab)
+  return tab == vim.api.nvim_get_current_tabpage()
 end
 
 GRIM.Tabline = function()
@@ -30,24 +43,18 @@ GRIM.Tabline = function()
   local tabs = vim.api.nvim_list_tabpages()
   for i, tab in ipairs(tabs) do
     local wins_visible = get_normal_wins(tab)
-    local tab_win_count = #wins_visible
-    local is_active = (tab == vim.api.nvim_get_current_tabpage())
-    local win_current = is_active and vim.api.nvim_get_current_win()
+    local count_tab_wins = #wins_visible
+    local win_current = tab_active_p(tab) and vim.api.nvim_get_current_win()
+      or last_active_win[tab]
       or wins_visible[1]
-    local buf_active = vim.api.nvim_win_get_buf(
-      win_current or vim.api.nvim_tabpage_list_wins(tab)[1]
-    )
-    local buf_active_name = get_buf_name(buf_active)
-    local modified = vim.tbl_contains(
-      vim.tbl_map(
-        function(win) return vim.bo[vim.api.nvim_win_get_buf(win)].modified end,
-        wins_visible
-      ),
-      true
-    )
-    local hl_left = is_active and "%#GRIMActiveLeft#" or "%#GRIMInactiveLeft#"
-    local hl_text = is_active and "%#GRIMActiveText#" or "%#GRIMInactiveText#"
-    local hl_right = is_active and "%#GRIMActiveRight#"
+    local buf_active = vim.api.nvim_win_get_buf(win_current or wins_visible[1])
+    local name_buf_active = get_buf_name(buf_active)
+    local buf_modified = vim.bo[buf_active].modified
+    local hl_left = tab_active_p(tab) and "%#GRIMActiveLeft#"
+      or "%#GRIMInactiveLeft#"
+    local hl_text = tab_active_p(tab) and "%#GRIMActiveText#"
+      or "%#GRIMInactiveText#"
+    local hl_right = tab_active_p(tab) and "%#GRIMActiveRight#"
       or "%#GRIMInactiveRight#"
     tabline = tabline .. hl_left .. " "
     tabline = tabline
@@ -55,28 +62,29 @@ GRIM.Tabline = function()
       .. i
       .. "."
       .. " "
-      .. buf_active_name
+      .. name_buf_active
       .. " "
-      .. (tab_win_count > 1 and "[" .. tab_win_count .. "] " or "")
-      .. (modified and S.Icons.ui.dot or " ")
+      .. (count_tab_wins > 1 and "[" .. count_tab_wins .. "] " or "")
+      .. (buf_modified and S.Icons.ui.dot or " ")
       .. " "
       .. "|"
     tabline = tabline .. hl_right .. ""
-    tabline = tabline .. "%#TabLine# "
+    tabline = tabline .. "%#TabLine#"
   end
   return tabline
 end
 
 GRIM.Highlight = function()
   local rp = require("clrs.road").palette
-  vim.api.nvim_set_hl(0, "TabLine", { fg = "#666666", bg = "NONE" })
-  vim.api.nvim_set_hl(0, "TabLineFill", { bg = "NONE" })
-  vim.api.nvim_set_hl(0, "GRIMActiveLeft", { fg = "NONE", bg = "NONE" })
-  vim.api.nvim_set_hl(0, "GRIMActiveText", { fg = rp.lotusYellow["DEFAULT"], bg = "NONE" })
-  vim.api.nvim_set_hl(0, "GRIMActiveRight", { fg = "NONE", bg = "NONE" })
-  vim.api.nvim_set_hl(0, "GRIMInactiveLeft", { fg = "NONE", bg = "NONE" })
-  vim.api.nvim_set_hl(0, "GRIMInactiveText", { fg = rp.charcoal[600], bg = "NONE" })
-  vim.api.nvim_set_hl(0, "GRIMInactiveRight", { fg = "NONE", bg = "NONE" })
+  local hl = vim.api.nvim_set_hl
+  hl(0, "TabLine", { fg = "#666666", bg = "NONE" })
+  hl(0, "TabLineFill", { bg = "NONE" })
+  hl(0, "GRIMActiveLeft", { fg = "NONE", bg = "NONE" })
+  hl(0, "GRIMActiveText", { fg = rp.lotusYellow["DEFAULT"], bg = "NONE" })
+  hl(0, "GRIMActiveRight", { fg = "NONE", bg = "NONE" })
+  hl(0, "GRIMInactiveLeft", { fg = "NONE", bg = "NONE" })
+  hl(0, "GRIMInactiveText", { fg = rp.charcoal[700], bg = "NONE" })
+  hl(0, "GRIMInactiveRight", { fg = "NONE", bg = "NONE" })
 end
 
 GRIM.Init = function()
