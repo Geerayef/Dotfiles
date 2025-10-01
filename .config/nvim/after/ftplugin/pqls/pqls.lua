beae7e9e485d87ae649f4ee5e19fc68d80c63575
Tibor Novakovic <tibor.novakovic@isis-papyrus.com>
2025-10-01 13:17:07 +0200

    feat: execution script

--- .config/nvim/after/ftplugin/pqls/pqls.lua
+++ .config/nvim/after/ftplugin/pqls/pqls.lua
@@ -1,3 +1,12 @@
 vim.treesitter.language.add("pqls")
 vim.treesitter.language.register("pqls", { "pqls" })
 vim.treesitter.start()
+-- ~ Options
+vim.opt.commentstring = "// %s"
+-- ~ Keymaps
+vim.keymap.set(
+  "n",
+  "<leader>PQL",
+  function() require("util.pql")(vim.api.nvim_get_current_buf()) end,
+  { buffer = true, desc = "Execute current PQLS buffer." }
+)
